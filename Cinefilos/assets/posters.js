d74d909c98494aba31f4176f2b089c76a6c518ff
qwertyboard3 <qwertyboard_2@hotmail.com>
2025-08-12 08:02:11 -0700

    Update posters.js

--- Cinefilos/assets/posters.js
+++ Cinefilos/assets/posters.js
@@ -27,6 +27,27 @@ const localFallback = {
   tt0114746: "posters/tt0114746.jpg", // 12 Monkeys
   tt0134817: "posters/tt0134817.jpg", // Mecánica Nacional
   tt0369339: "posters/tt0369339.jpg", // Collateral
-
+//
+  tt7131622: "posters/tt7131622.jpg", // Once Upon a Time... in Hollywood
+  tt0162346: "posters/tt0162346.jpg", // Ghost World
+  tt0128445: "posters/tt0128445.jpg", // Rushmore
+  tt0069089: "posters/tt0069089.jpg", // Pink Flamingos
+  tt0100935: "posters/tt0100935.jpg", // Wild at Heart
+  tt0340855: "posters/tt0340855.jpg", // Monster
+  tt0377091: "posters/tt0377091.jpg", // Mean Creek
+  tt0114906: "posters/tt0114906.jpg", // Welcome to the Dollhouse
+  tt3499048: "posters/tt3499048.jpg", // Gueros
+  tt0052357: "posters/tt0052357.jpg", // Vertigo
+  tt0070544: "posters/tt0070544.jpg", // Fantastic Planet
+  tt0298203: "posters/tt0298203.jpg", // 8 Mile
+  tt0208502: "posters/tt0208502.jpg", // Angel's Egg
+  tt0481369: "posters/tt0481369.jpg", // The Number 23
+  tt0078480: "posters/tt0078480.jpg", // Watership Down
+  tt0225009: "posters/tt0225009.jpg", // 964 Pinocchio
+  tt0101698: "posters/tt0101698.jpg", // Defending Your Life
+  tt0139239: "posters/tt0139239.jpg", // Go
+  tt0110630: "posters/tt0110630.jpg", // The Friends
+  tt0010323: "posters/tt0010323.jpg", // The Cabinet of Dr. Caligari
 };
+
 
