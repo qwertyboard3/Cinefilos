5a647f365f8793e61c0c246853271f82e220b4a4
qwertyboard3 <qwertyboard_2@hotmail.com>
2025-08-12 11:25:21 -0700

    Update posters.js

--- Cinefilos/assets/posters.js
+++ Cinefilos/assets/posters.js
@@ -48,6 +48,180 @@ const localFallback = {
   tt0139239: "posters/tt0139239.jpg", // Go
   tt0110630: "posters/tt0110630.jpg", // The Friends
   tt0010323: "posters/tt0010323.jpg", // The Cabinet of Dr. Caligari
+//
+  tt1260944: "posters/tt1260944.jpg", // Les Résultats du féminisme
+  tt0015864: "posters/tt0015864.jpg", // The Gold Rush
+  tt0113855: "posters/tt0113855.jpg", // The Lustrous Pearl
+  tt0006864: "posters/tt0006864.jpg", // Intolerance
+  tt0012349: "posters/tt0012349.jpg", // The Kid
+  tt0017136: "posters/tt0017136.jpg", // Metropolis
+  tt0019421: "posters/tt0019421.jpg", // Steamboat Bill
+  tt0017775: "posters/tt0017775.jpg", // The Seashell and the Clergyman
+  tt0021577: "posters/tt0021577.jpg", // L'Âge d'Or
+  tt0021331: "posters/tt0021331.jpg", // The Blood of a Poet
+  tt0176049: "posters/tt0176049.jpg", // Prisioneros de la tierra
+  tt0034820: "posters/tt0034820.jpg", // La guerra gaucha
+  tt0034583: "posters/tt0034583.jpg", // Casablanca
+  tt0038823: "posters/tt0038823.jpg", // Paisà
+  tt0038109: "posters/tt0038109.jpg", // Spellbound
+  tt0031885: "posters/tt0031885.jpg", // The Rules of the Game
+  tt0032455: "posters/tt0032455.jpg", // Fantasia
+  tt0027977: "posters/tt0027977.jpg", // Modern Times
+  tt0040522: "posters/tt0040522.jpg", // Bicycle Thieves
+  tt0047445: "posters/tt0047445.jpg", // Sansho the Bailiff
+  tt0048500: "posters/tt0048500.jpg", // A Generation
+  tt0049902: "posters/tt0049902.jpg", // A Man Escaped
+  tt0053168: "posters/tt0053168.jpg", // Pickpocket
+  tt0040746: "posters/tt0040746.jpg", // Rope
+  tt0056736: "posters/tt0056736.jpg", // L'Eclisse
+  tt0052893: "posters/tt0052893.jpg", // Hiroshima Mon Amour
+  tt0053121: "posters/tt0053121.jpg", // Fires on the Plain
+  tt0056196: "posters/tt0056196.jpg", // Long Day's Journey into Night
+  tt0048452: "posters/tt0048452.jpg", // Ordet
+  tt0053472: "posters/tt0053472.jpg", // Breathless
+  tt0054632: "posters/tt0054632.jpg", // Last Year at Marienbad
+  tt0050986: "posters/tt0050986.jpg", // Wild Strawberries
+  tt0052080: "posters/tt0052080.jpg", // Ashes and Diamonds
+  tt0045230: "posters/tt0045230.jpg", // The Thief
+  tt0060138: "posters/tt0060138.jpg", // Au hasard Balthazar
+  tt0063084: "posters/tt0063084.jpg", // La hora de los hornos
+  tt0058946: "posters/tt0058946.jpg", // The Battle of Algiers
+  tt0057977: "posters/tt0057977.jpg", // Crónica de un niño solo
+  tt0060107: "posters/tt0060107.jpg", // Andrei Rublev
+  tt0063555: "posters/tt0063555.jpg", // The Color of Pomegranates
+  tt0065214: "posters/tt0065214.jpg", // The Wild Bunch
+  tt0070544: "posters/tt0070544.jpg", // Fantastic Planet
+  tt0067116: "posters/tt0067116.jpg", // The French Connection
+  tt0059807: "posters/tt0059807.jpg", // Time Piece
+  tt0061177: "posters/tt0061177.jpg", // What's Up
+  tt0066919: "posters/tt0066919.jpg", // La classe operaia va in paradiso
+  tt0063493: "posters/tt0063493.jpg", // The Reconstruction
+  tt0064356: "posters/tt0064356.jpg", // The Cow
+  tt0123885: "posters/tt0123885.jpg", // La hermana Trinquete
+  tt0066530: "posters/tt0066530.jpg", // Viva la Muerte
+  tt0071129: "posters/tt0071129.jpg", // Amarcord
+  tt0069293: "posters/tt0069293.jpg", // Solaris
+  tt0068361: "posters/tt0068361.jpg", // The Discreet Charm of the Bourgeoisie
+  tt0068182: "posters/tt0068182.jpg", // Aguirre
+  tt0070235: "posters/tt0070235.jpg", // I Will Walk Like a Crazy Horse
+  tt0070250: "posters/tt0070250.jpg", // Juan Moreira
+  tt0071615: "posters/tt0071615.jpg", // The Holy Mountain
+  tt0070707: "posters/tt0070707.jpg", // Sleeper
+  tt0072684: "posters/tt0072684.jpg", // Barry Lyndon
+  tt0071315: "posters/tt0071315.jpg", // Chinatown
+  tt0071737: "posters/tt0071737.jpg", // Lancelot du Lac
+  tt0072355: "posters/tt0072355.jpg", // Vase de Noces
+  tt0072443: "posters/tt0072443.jpg", // Mirror
+  tt0073650: "posters/tt0073650.jpg", // Salò
+  tt0075222: "posters/tt0075222.jpg", // Silent Movie
+  tt0076786: "posters/tt0076786.jpg", // Suspiria
+  tt0075666: "posters/tt0075666.jpg", // Alucarda
+  tt0075726: "posters/tt0075726.jpg", // Barbara Broadcast
+  tt0071897: "posters/tt0071897.jpg", // Nazareno Cruz y el lobo
+  tt0075824: "posters/tt0075824.jpg", // That Obscure Object of Desire
+  tt0079020: "posters/tt0079020.jpg", // ¡Que viva México!
+  tt0082222: "posters/tt0082222.jpg", // Man of Iron
+  tt0085809: "posters/tt0085809.jpg", // Koyaanisqatsi
+  tt0085180: "posters/tt0085180.jpg", // L'argent
+  tt0088846: "posters/tt0088846.jpg", // Brazil
+  tt0091214: "posters/tt0091214.jpg", // Hombre mirando al sudeste
+  tt0095715: "posters/tt0095715.jpg", // Alice
+  tt0081881: "posters/tt0081881.jpg", // Jarinko Chie
+  tt0086541: "posters/tt0086541.jpg", // Videodrome
+  tt0083560: "posters/tt0083560.jpg", // The Angel
+  tt0092337: "posters/tt0092337.jpg", // Dekalog
+  tt0084362: "posters/tt0084362.jpg", // Dimensions of Dialogue
+  tt0087144: "posters/tt0087144.jpg", // La diagonal del loco
+  tt0089078: "posters/tt0089078.jpg", // El Elegido
+  tt0097228: "posters/tt0097228.jpg", // Dr. Caligari
+  tt0101410: "posters/tt0101410.jpg", // Barton Fink
+  tt0102478: "posters/tt0102478.jpg", // La mujer de Benjamín
+  tt0102511: "posters/tt0102511.jpg", // Naked Lunch
+  tt0101765: "posters/tt0101765.jpg", // The Double Life of Véronique
+  tt0104285: "posters/tt0104285.jpg", // Food
+  tt0104684: "posters/tt0104684.jpg", // Hard Boiled
+  tt0104797: "posters/tt0104797.jpg", // Malcolm X
+  tt0104662: "posters/tt0104662.jpg", // The Dark Side of the Heart
+  tt0108149: "posters/tt0108149.jpg", // Six Degrees of Separation
+  tt0108394: "posters/tt0108394.jpg", // Three Colors: Blue
+  tt0111507: "posters/tt0111507.jpg", // Three Colors: White
+  tt0111495: "posters/tt0111495.jpg", // Three Colors: Red
+  tt0109424: "posters/tt0109424.jpg", // Chungking Express
+  tt0896690: "posters/tt0896690.jpg", // El Alimento del miedo
+  tt0113627: "posters/tt0113627.jpg", // Leaving Las Vegas
+  tt1992236: "posters/tt1992236.jpg", // The Cremaster Cycle
+  tt0110081: "posters/tt0110081.jpg", // To Live
+  tt0114323: "posters/tt0114323.jpg", // Safe
+  tt0117604: "posters/tt0117604.jpg", // Seven Servants
+  tt1910653: "posters/tt1910653.jpg", // Triste
+  tt0119167: "posters/tt0119167.jpg", // Funny Games
+  tt0120265: "posters/tt0120265.jpg", // Taste of Cherry
+  tt0119715: "posters/tt0119715.jpg", // Mouse Hunt
+  tt0120863: "posters/tt0120863.jpg", // The Thin Red Line
+  tt0118715: "posters/tt0118715.jpg", // The Big Lebowski
+  tt0120601: "posters/tt0120601.jpg", // Being John Malkovich
+  tt0154506: "posters/tt0154506.jpg", // Following
+  tt0162023: "posters/tt0162023.jpg", // Tuvalu
+  tt17068914: "posters/tt17068914.jpg", // Jorge Luis Borges: Una vida de poesía
+  tt0266308: "posters/tt0266308.jpg", // Battle Royale
+  tt0272152: "posters/tt0272152.jpg", // K-PAX
+  tt0228687: "posters/tt0228687.jpg", // Little Otik
+  tt0165929: "posters/tt0165929.jpg", // Romeo Must Die
+  tt0220100: "posters/tt0220100.jpg", // Timecode
+  tt0268978: "posters/tt0268978.jpg", // A Beautiful Mind
+  tt0246578: "posters/tt0246578.jpg", // Donnie Darko
+  tt0240419: "posters/tt0240419.jpg", // La Ciénaga
+  tt0272338: "posters/tt0272338.jpg", // Punch-Drunk Love
+  tt0338337: "posters/tt0338337.jpg", // Paycheck
+  tt0390384: "posters/tt0390384.jpg", // Primer
+  tt0456149: "posters/tt0456149.jpg", // The Death of Mr. Lazarescu
+  tt0377771: "posters/tt0377771.jpg", // Destino
+  tt0365485: "posters/tt0365485.jpg", // Domino
+  tt0459666: "posters/tt0459666.jpg", // Three Times
+  tt0460829: "posters/tt0460829.jpg", // Inland Empire
+  tt0120263: "posters/tt0120263.jpg", // Songs from the Second Floor
+  tt0354899: "posters/tt0354899.jpg", // The Science of Sleep
+  tt0947798: "posters/tt0947798.jpg", // Black Swan
+  tt1278322: "posters/tt1278322.jpg", // Colin
+  tt0383028: "posters/tt0383028.jpg", // Synecdoche
+  tt1022603: "posters/tt1022603.jpg", // 500 Days of Summer
+  tt1191111: "posters/tt1191111.jpg", // Enter the Void
+  tt1534085: "posters/tt1534085.jpg", // Beyond the Black Rainbow
+  tt1375666: "posters/tt1375666.jpg", // Inception
+  tt1268969: "posters/tt1268969.jpg", // Kill Your Idols
+  tt1130884: "posters/tt1130884.jpg", // Shutter Island
+  tt1813757: "posters/tt1813757.jpg", // Who Killed Captain Alex?
+  tt0780504: "posters/tt0780504.jpg", // Drive
+  tt1527186: "posters/tt1527186.jpg", // Melancholia
+  tt1605783: "posters/tt1605783.jpg", // Midnight in Paris
+  tt1655442: "posters/tt1655442.jpg", // The Artist
+  tt0478304: "posters/tt0478304.jpg", // The Tree of Life
+  tt2076220: "posters/tt2076220.jpg", // Holy Motors
+  tt1441395: "posters/tt1441395.jpg", // Under the Skin
+  tt2562232: "posters/tt2562232.jpg", // Birdman
+  tt3076658: "posters/tt3076658.jpg", // Creed
+  tt1951524: "posters/tt1951524.jpg", // Sanguivorous
+  tt2316411: "posters/tt2316411.jpg", // Enemy
+  tt1854513: "posters/tt1854513.jpg", // Blancanieves
+  tt3568002: "posters/tt3568002.jpg", // The Witness
+  tt4226388: "posters/tt4226388.jpg", // Victoria
+  tt8579674: "posters/tt8579674.jpg", // 1917
+  tt6833964: "posters/tt6833964.jpg", // Sonora
+  tt9451096: "posters/tt9451096.jpg", // Workforce
+  tt9777644: "posters/tt9777644.jpg", // Da 5 Bloods
+  tt6074542: "posters/tt6074542.jpg", // Friend of the World
+  tt21905940: "posters/tt21905940.jpg", // El Castigo
+  tt17519342: "posters/tt17519342.jpg", // Life and Death in the Warehouse
+  tt11866324: "posters/tt11866324.jpg", // Prey
+  tt23769666: "posters/tt23769666.jpg", // 1984
+  tt19880934: "posters/tt19880934.jpg", // Failure!
+  tt13686040: "posters/tt13686040.jpg", // Quarantine
+  tt34463310: "posters/tt34463310.jpg", // Detective Chinatown 1900
+  tt34956443: "posters/tt34956443.jpg", // Ne Zha 2
+  tt13462900: "posters/tt13462900.jpg", // The Battle at Lake Changjin
+  tt26675379: "posters/tt26675379.jpg", // Vitória
+  tt2184331: "posters/tt2184331.jpg", // Una noche
 };
+
 
 
