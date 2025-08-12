6d30f1351a63306fa50d9e3b79ba0fe0edcd8320
qwertyboard3 <qwertyboard_2@hotmail.com>
2025-08-12 11:26:00 -0700

    Update runtime.js

--- Cinefilos/assets/runtime.js
+++ Cinefilos/assets/runtime.js
@@ -48,4 +48,178 @@ const forcedRuntime = {
   "tt0139239": "102 min", // Go
   "tt0110630": "113 min", // The Friends
   "tt0010323": "67 min", // The Cabinet of Dr. Caligari  
+  //
+    "tt1260944": "7 min", // Les Résultats du féminisme
+  "tt0015864": "95 min", // The Gold Rush
+  "tt0113855": "101 min", // The Lustrous Pearl
+  "tt0006864": "163 min", // Intolerance
+  "tt0012349": "68 min", // The Kid
+  "tt0017136": "153 min", // Metropolis
+  "tt0019421": "70 min", // Steamboat Bill
+  "tt0017775": "40 min", // The Seashell and the Clergyman
+  "tt0021577": "63 min", // L'Âge d'Or
+  "tt0021331": "55 min", // The Blood of a Poet
+  "tt0176049": "85 min", // Prisioneros de la tierra
+  "tt0034820": "90 min", // La guerra gaucha
+  "tt0034583": "102 min", // Casablanca
+  "tt0038823": "120 min", // Paisà
+  "tt0038109": "111 min", // Spellbound
+  "tt0031885": "110 min", // The Rules of the Game
+  "tt0032455": "124 min", // Fantasia
+  "tt0027977": "87 min", // Modern Times
+  "tt0040522": "89 min", // Bicycle Thieves
+  "tt0047445": "124 min", // Sansho the Bailiff
+  "tt0048500": "87 min", // A Generation
+  "tt0049902": "101 min", // A Man Escaped
+  "tt0053168": "76 min", // Pickpocket
+  "tt0040746": "80 min", // Rope
+  "tt0056736": "126 min", // L'Eclisse
+  "tt0052893": "90 min", // Hiroshima Mon Amour
+  "tt0053121": "108 min", // Fires on the Plain
+  "tt0056196": "174 min", // Long Day's Journey into Night
+  "tt0048452": "126 min", // Ordet
+  "tt0053472": "90 min", // Breathless
+  "tt0054632": "94 min", // Last Year at Marienbad
+  "tt0050986": "92 min", // Wild Strawberries
+  "tt0052080": "103 min", // Ashes and Diamonds
+  "tt0045230": "86 min", // The Thief
+  "tt0060138": "95 min", // Au hasard Balthazar
+  "tt0063084": "260 min", // La hora de los hornos
+  "tt0058946": "121 min", // The Battle of Algiers
+  "tt0057977": "79 min", // Crónica de un niño solo
+  "tt0060107": "189 min", // Andrei Rublev
+  "tt0063555": "79 min", // The Color of Pomegranates
+  "tt0065214": "135 min", // The Wild Bunch
+  "tt0070544": "72 min", // Fantastic Planet
+  "tt0067116": "104 min", // The French Connection
+  "tt0059807": "9 min", // Time Piece
+  "tt0061177": "80 min", // What's Up
+  "tt0066919": "125 min", // La classe operaia va in paradiso
+  "tt0063493": "100 min", // The Reconstruction
+  "tt0064356": "104 min", // The Cow
+  "tt0123885": "87 min", // La hermana Trinquete
+  "tt0066530": "90 min", // Viva la Muerte
+  "tt0071129": "123 min", // Amarcord
+  "tt0069293": "167 min", // Solaris
+  "tt0068361": "102 min", // The Discreet Charm of the Bourgeoisie
+  "tt0068182": "95 min", // Aguirre
+  "tt0070235": "100 min", // I Will Walk Like a Crazy Horse
+  "tt0070250": "102 min", // Juan Moreira
+  "tt0071615": "114 min", // The Holy Mountain
+  "tt0070707": "89 min", // Sleeper
+  "tt0072684": "185 min", // Barry Lyndon
+  "tt0071315": "130 min", // Chinatown
+  "tt0071737": "85 min", // Lancelot du Lac
+  "tt0072355": "80 min", // Vase de Noces
+  "tt0072443": "107 min", // Mirror
+  "tt0073650": "117 min", // Salò
+  "tt0075222": "87 min", // Silent Movie
+  "tt0076786": "92 min", // Suspiria
+  "tt0075666": "78 min", // Alucarda
+  "tt0075726": "82 min", // Barbara Broadcast
+  "tt0071897": "92 min", // Nazareno Cruz y el lobo
+  "tt0075824": "103 min", // That Obscure Object of Desire
+  "tt0079020": "90 min", // ¡Que viva México!
+  "tt0082222": "156 min", // Man of Iron
+  "tt0085809": "86 min", // Koyaanisqatsi
+  "tt0085180": "85 min", // L'argent
+  "tt0088846": "132 min", // Brazil
+  "tt0091214": "105 min", // Hombre mirando al sudeste
+  "tt0095715": "86 min", // Alice
+  "tt0081881": "105 min", // Jarinko Chie
+  "tt0086541": "87 min", // Videodrome
+  "tt0083560": "92 min", // The Angel
+  "tt0092337": "572 min", // Dekalog
+  "tt0084362": "12 min", // Dimensions of Dialogue
+  "tt0087144": "100 min", // La diagonal del loco
+  "tt0089078": "80 min", // El Elegido
+  "tt0097228": "80 min", // Dr. Caligari
+  "tt0101410": "116 min", // Barton Fink
+  "tt0102478": "90 min", // La mujer de Benjamín
+  "tt0102511": "115 min", // Naked Lunch
+  "tt0101765": "98 min", // The Double Life of Véronique
+  "tt0104285": "17 min", // Food
+  "tt0104684": "128 min", // Hard Boiled
+  "tt0104797": "202 min", // Malcolm X
+  "tt0104662": "127 min", // The Dark Side of the Heart
+  "tt0108149": "112 min", // Six Degrees of Separation
+  "tt0108394": "98 min", // Three Colors: Blue
+  "tt0111507": "92 min", // Three Colors: White
+  "tt0111495": "99 min", // Three Colors: Red
+  "tt0109424": "102 min", // Chungking Express
+  "tt0896690": "130 min", // El Alimento del miedo
+  "tt0113627": "111 min", // Leaving Las Vegas
+  "tt1992236": "398 min", // The Cremaster Cycle
+  "tt0110081": "133 min", // To Live
+  "tt0114323": "119 min", // Safe
+  "tt0117604": "88 min", // Seven Servants
+  "tt1910653": "19 min", // Triste
+  "tt0119167": "108 min", // Funny Games
+  "tt0120265": "95 min", // Taste of Cherry
+  "tt0119715": "98 min", // Mouse Hunt
+  "tt0120863": "170 min", // The Thin Red Line
+  "tt0118715": "117 min", // The Big Lebowski
+  "tt0120601": "113 min", // Being John Malkovich
+  "tt0154506": "69 min", // Following
+  "tt0162023": "101 min", // Tuvalu
+  "tt17068914": "65 min", // Jorge Luis Borges: Una vida de poesía
+  "tt0266308": "114 min", // Battle Royale
+  "tt0272152": "120 min", // K-PAX
+  "tt0228687": "132 min", // Little Otik
+  "tt0165929": "115 min", // Romeo Must Die
+  "tt0220100": "97 min", // Timecode
+  "tt0268978": "135 min", // A Beautiful Mind
+  "tt0246578": "113 min", // Donnie Darko
+  "tt0240419": "103 min", // La Ciénaga
+  "tt0272338": "95 min", // Punch-Drunk Love
+  "tt0338337": "119 min", // Paycheck
+  "tt0390384": "77 min", // Primer
+  "tt0456149": "153 min", // The Death of Mr. Lazarescu
+  "tt0377771": "70 min", // Destino
+  "tt0365485": "96 min", // Domino
+  "tt0459666": "120 min", // Three Times
+  "tt0460829": "180 min", // Inland Empire
+  "tt0120263": "98 min", // Songs from the Second Floor
+  "tt0354899": "105 min", // The Science of Sleep
+  "tt0947798": "108 min", // Black Swan
+  "tt1278322": "97 min", // Colin
+  "tt0383028": "124 min", // Synecdoche
+  "tt1022603": "95 min", // 500 Days of Summer
+  "tt1191111": "161 min", // Enter the Void
+  "tt1534085": "110 min", // Beyond the Black Rainbow
+  "tt1375666": "148 min", // Inception
+  "tt1268969": "57 min", // Kill Your Idols
+  "tt1130884": "138 min", // Shutter Island
+  "tt1813757": "64 min", // Who Killed Captain Alex?
+  "tt0780504": "100 min", // Drive
+  "tt1527186": "135 min", // Melancholia
+  "tt1605783": "94 min", // Midnight in Paris
+  "tt1655442": "100 min", // The Artist
+  "tt0478304": "139 min", // The Tree of Life
+  "tt2076220": "115 min", // Holy Motors
+  "tt1441395": "108 min", // Under the Skin
+  "tt2562232": "119 min", // Birdman
+  "tt3076658": "133 min", // Creed
+  "tt1951524": "45 min", // Sanguivorous
+  "tt2316411": "91 min", // Enemy
+  "tt1854513": "104 min", // Blancanieves
+  "tt3568002": "89 min", // The Witness
+  "tt4226388": "138 min", // Victoria
+  "tt8579674": "119 min", // 1917
+  "tt6833964": "94 min", // Sonora
+  "tt9451096": "82 min", // Workforce
+  "tt9777644": "154 min", // Da 5 Bloods
+  "tt6074542": "50 min", // Friend of the World
+  "tt21905940": "85 min", // El Castigo
+  "tt17519342": "59 min", // Life and Death in the Warehouse
+  "tt11866324": "100 min", // Prey
+  "tt23769666": "133 min", // 1984
+  "tt19880934": "87 min", // Failure!
+  "tt13686040": "74 min", // Quarantine
+  "tt34463310": "136 min", // Detective Chinatown 1900
+  "tt34956443": "143 min", // Ne Zha 2
+  "tt13462900": "176 min", // The Battle at Lake Changjin
+  "tt26675379": "N/A", // Vitória
+  "tt2184331": "90 min", // Una noche
 };
+
